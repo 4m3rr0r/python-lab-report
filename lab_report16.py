652e59f72ac372facf18c4cbc950aaee39403112
Sheikh Mohammad Hasan <90191914+4m3rr0r@users.noreply.github.com>
2024-11-04 00:32:00 +0600

    Update lab_report16.py

--- lab_report16.py
+++ lab_report16.py
@@ -1,15 +1,14 @@
-number = int(input("Enter a positive integer: "))
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-if number < 2:
-    print(number, "is not a prime number.")
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
 else:
-    is_prime = True
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            is_prime = False
-            break
-
-    if is_prime:
-        print(number, "is a prime number.")
-    else:
-        print(number, "is not a prime number.")
+    print(f"{number} is not a prime number.")
